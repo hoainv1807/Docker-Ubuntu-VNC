328f7461f4d356a72858f0f05c3fc0db589b0b74
hoainv1807 <47892115+hoainv1807@users.noreply.github.com>
2025-05-06 17:16:35 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,26 @@ if [ ! -f "/root/.vnc/passwd" ]; then
     chmod 600 /root/.vnc/passwd
 fi
 
+echo " "
+echo "Stopping any existing VNC server processes..."
+pkill -f "Xvnc" || echo "No existing VNC processes found."
+
+echo "Stopping any existing VNC server processes..."
+pkill -f "Xvnc" || echo "No existing VNC processes found."
+
+echo " "
+echo "Checking for and removing stale VNC lock and temporary files..."
+if [ -f /tmp/.X1-lock ]; then
+    echo " "
+    echo "Found stale /tmp/.X1-lock file. Removing it..."
+    rm -f /tmp/.X1-lock
+fi
+if [ -d /tmp/.X11-unix ]; then
+    echo " "
+    echo "Removing stale /tmp/.X11-unix directory..."
+    rm -rf /tmp/.X11-unix
+fi
+
 vncserver :1 -geometry 680x820 -depth 16
 
 tail -f /dev/null
